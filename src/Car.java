c795360c121940a494b425946defa623fa437df0
Marija <marija.kovacic.bg@gmail.com>
2018-05-28 12:20:10 +0200

    promena broja putnika

--- src/Car.java
+++ src/Car.java
@@ -1,81 +1,123 @@
+
 public class Car {
 
-String model;
-int maxSpeed;
-int minSpeed;
-double weight;
+    String model;
+    int maxSpeed;
+    int minSpeed;
+    double weight;
 
-String licence = "abc123";
-int maxFuel;
-int currentFuel;
-int consumption; // consumption per distance unit (km)
-int mileage = 0;
+    String licence = "abc123";
+    int maxFuel;
+    int currentFuel;
+    int consumption; // consumption per distance unit (km)
+    int mileage = 0;
 
-public Car() {
+    int maxPassengers;
+    int curentPassengers;
+
+    public Car() {
         this.model = "defauld";
         this.maxFuel = 100;
         this.consumption = 5;
-
-}
+        this.maxPassengers = 5;
+        this.curentPassengers = 1;
+    }
 
     public Car(String customModel, int customMaxFuel, int customConsumption) {
-        
+
         this.model = customModel;
         this.maxFuel = customMaxFuel;
         this.consumption = customConsumption;
-        
+
     }
-    
+
+    public Car(String customModel, int customMaxFuel, int customConsumption, int customMaxPassengers, int customCurrentPassengers) {
+
+        this.model = customModel;
+        this.maxFuel = customMaxFuel;
+        this.consumption = customConsumption;
+        this.maxPassengers = customMaxPassengers;
+        this.curentPassengers = customCurrentPassengers;
+        
+
+    }
+
     public void printAttributes() {
-    
+
         System.out.println("Model: " + this.model);
         System.out.println("Max Fuel: " + this.maxFuel);
         System.out.println("Consumprion: " + this.consumption);
         System.out.println("Licence: " + this.licence);
         System.out.println("Current fuel: " + this.currentFuel);
         System.out.println("Distance: " + this.mileage);
-        
-    }
+        System.out.println("Max broj putnika: " + this.maxPassengers);
+        System.out.println("Trenutni broj putnika: " + this.curentPassengers);
+        System.out.println();
 
+    }
 
     public void changeLicence(String customLicence) {
         this.licence = customLicence;
     }
 
-    public void changeWeight (int newWeight) {
+    public void changeWeight(int newWeight) {
         this.weight = newWeight;
     }
-    
+
     public double maxDistance() {
         double dist = this.maxFuel / this.consumption;
         return dist;
-        
+
         //return this.maxFuel / this.consumption
     }
-    
+
     public void fuelUp() {
         this.currentFuel = this.maxFuel;
-        
+
     }
-    
+
     public void travel(int distance) {
-        
+
         //ako ima dovoljno goriva za put putuj (mileage, smanji trenutno gorivo)
         // ako nema dovoljno goriva, poruka nema dovoljno goriva
-        
-        if (this.currentFuel  >= distance * this.consumption) {
+        if (this.currentFuel >= distance * this.consumption) {
             this.mileage = this.mileage + distance;
-        this.currentFuel = this.currentFuel - distance * this.consumption;
+            this.currentFuel = this.currentFuel - distance * this.consumption;
         } else {
             System.out.println("Nema dovoljno goriva");
         }
-            
-        
+
         this.mileage = this.mileage + distance;
         this.currentFuel = this.currentFuel - distance * this.consumption;
     }
 
     
+    
+    
+    
+    public void getIn(int ulazak) {
+              
+        if (this.curentPassengers + ulazak < this.maxPassengers){
+            this.curentPassengers = this.curentPassengers + ulazak;
+            System.out.println("ima jos mesta\n");
+        } else {
+            System.out.println("nema dovoljno mesta");
+        }
+    }
+
+    public void getOut(int izlazak) {
+        
+        if (this.curentPassengers - izlazak > 0) {
+            this.curentPassengers = this.curentPassengers - izlazak;
+            System.out.println("ima jos mesta");
+        } else {
+            System.out.println("nema dovoljno mesta");
+        }
+        
+    }   
+    
+
 }
 
+ 
 
--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,40 @@ public class Main {
     
     public static void main(String[] args) {
        
-        Car myCar = new Car();
-//      myCar.printAttributes();
+//        Car myCar = new Car();
+//        myCar.printAttributes();
 //        myCar.changeLicence("efg123");
 //        myCar.printAttributes();
 //        double distance = myCar.maxDistance();
 //        System.out.println("Max distanca: " + distance);
         
-        Car audi = new Car("Audi", 50, 2);
-        audi.printAttributes();
+//        Car audi = new Car("Audi", 50, 2);
+//        audi.printAttributes();
 //        audi.changeLicence("xzy123");
 //        audi.printAttributes();
 //        System.out.println("Max distanca: " + audi.maxDistance());
 
-        audi.fuelUp();
-        audi.travel(20);
-        audi.printAttributes();
-        audi.travel(30);
-        audi.printAttributes();
-    }
+//        audi.fuelUp();
+//        audi.travel(20);
+//        audi.printAttributes();
+//        audi.travel(30);
+//        audi.printAttributes();
+   
+
+        Car noviAuto = new Car();
+        noviAuto.printAttributes();
+        
+        noviAuto.getIn(2);
+        noviAuto.printAttributes();
+         noviAuto.printAttributes();
+        noviAuto.getOut(8);
+       
     
+    
+    
+    }
+   
+        
+        
+        
 }
